fd2039a7f9ae562b05b97310090a7ed2003283d0
Lukas Klein <lukas.klein@dkfz-heidelberg.de>
2021-09-28 17:27:27 +0200

    init

---
+++ src/models/head_mlp.py
@@ -0,0 +1,124 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.optim import Adam
+from torchmetrics.functional import confusion_matrix, accuracy
+
+import pytorch_lightning as pl
+
+from src.models.tcvae_resnet import *
+
+class MLP(pl.LightningModule):
+    def __init__(
+        self,
+        encoder_name: str,
+        data_dir: str,
+        latent_dim: int,
+        num_classes: int,
+        weight_decay: float,
+        lr = 0.0001,
+        fix_weights = True
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+
+        path_ckpt = data_dir + "/models/" + encoder_name + ".ckpt"
+        self.encoder = betaTCVAE_ResNet.load_from_checkpoint(path_ckpt)
+
+        if fix_weights == True:
+            self.encoder.freeze()
+        else:
+            self.encoder.eval()
+
+        self.fc1 = nn.Linear(self.hparams.latent_dim, 256, bias=True)
+        self.fc2 = nn.Linear(256, self.hparams.num_classes, bias=True)
+
+
+    def forward(self, x):
+
+        if x.shape[1] != self.hparams.latent_dim:
+            x = self.encoder(x)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        
+        y_hat = F.softmax(x, dim=1)
+
+        return y_hat
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+
+        y_hat = self(x)
+
+        loss = F.cross_entropy(y_hat, y, reduction='mean')
+
+        acc = accuracy(y_hat, y)
+
+        self.log('loss', loss, on_epoch=True,
+                 sync_dist=True if torch.cuda.device_count() > 1 else False)
+
+        self.log('acc', acc, on_epoch=False,  prog_bar=True,
+                 sync_dist=True if torch.cuda.device_count() > 1 else False)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+
+        y_hat = self(x)
+
+        val_loss = F.cross_entropy(y_hat, y, reduction='mean')
+
+        return {'val_loss': val_loss, 'val_y': y, 'val_y_hat': y_hat}
+
+    def validation_epoch_end(self, outputs):
+        val_y = torch.cat(tuple([x['val_y'] for x in outputs]))
+        val_y_hat = torch.cat(tuple([x['val_y_hat'] for x in outputs]))
+
+        val_loss = F.cross_entropy(val_y_hat, val_y, reduction='mean')
+        acc = accuracy(val_y_hat, val_y)
+
+        self.log('val_loss', val_loss, on_epoch=True, prog_bar=True,
+                 sync_dist=True if torch.cuda.device_count() > 1 else False)
+
+        self.log('val_acc', acc, on_epoch=True, prog_bar=True,
+                 sync_dist=True if torch.cuda.device_count() > 1 else False)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        
+        y_hat = self(x)
+
+        test_loss = F.cross_entropy(y_hat, y, reduction='mean')
+
+        return {'test_loss': test_loss, 'test_y': y, 'test_y_hat': y_hat}
+
+    def test_epoch_end(self, outputs):
+        test_y = torch.cat(tuple([x['test_y'] for x in outputs]))
+        test_y_hat = torch.cat(tuple([x['test_y_hat'] for x in outputs]))
+
+        test_loss = F.cross_entropy(test_y_hat, test_y, reduction='mean')
+
+        acc = accuracy(test_y_hat, test_y)
+        confmat = confusion_matrix(test_y_hat, test_y, num_classes=self.hparams.num_classes)
+
+        self.log('test_loss', test_loss, on_epoch=True, prog_bar=True,
+                 sync_dist=True if torch.cuda.device_count() > 1 else False)
+                 
+        self.log('test_acc', acc, on_epoch=True, prog_bar=True,
+                 sync_dist=True if torch.cuda.device_count() > 1 else False)
+
+        print("\n Confusion Matrix: \n", torch.round(confmat).type(torch.IntTensor))
+
+    def configure_optimizers(self):
+        optimizer = Adam(self.parameters(), lr=self.hparams.lr, weight_decay = self.hparams.weight_decay)
+
+        scheduler = {'scheduler': CosineAnnealingLR(optimizer,T_max=self.trainer.max_epochs, verbose=True), 
+                    'interval': 'epoch'}
+        return [optimizer], [scheduler] 
+
+    def get_progress_bar_dict(self):
+        tqdm_dict = super().get_progress_bar_dict()
+        if 'v_num' in tqdm_dict:
+            del tqdm_dict['v_num']
+        return tqdm_dict
---
+++ src/models/tcvae_resnet.py
@@ -0,0 +1,176 @@
+import math
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch import optim, Tensor
+
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+import torchvision
+import pytorch_lightning as pl
+from pl_bolts.models.autoencoders.basic_ae.basic_ae_module import AE
+
+class betaTCVAE_ResNet(pl.LightningModule):
+    def __init__(self,
+                 trainset_size=50000,
+                 latent_dim=10,
+                 input_dim=784,
+                 lr=0.001,
+                 momentum=0.9,
+                 weight_decay= 1e-4,
+                 anneal_steps: int = 200,
+                 alpha: float = 1.,
+                 beta: float = 1.,
+                 gamma: float = 1.
+                 ):
+        super(betaTCVAE_ResNet, self).__init__()
+        self.save_hyperparameters()
+        self.num_iter = 0
+
+        # Encoder
+        self.enc = AE(enc_type="resnet50", latent_dim=latent_dim, input_height = input_dim).encoder
+        self.enc.conv1 = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+
+        self.enc_fc = nn.Linear(in_features=512*4, out_features=256)
+        self.fc_mu = nn.Linear(in_features=256, out_features=latent_dim)
+        self.fc_logvar = nn.Linear(in_features=256, out_features=latent_dim)
+
+        # Decoder
+        self.dec = AE(latent_dim=latent_dim, input_height = input_dim).decoder
+        self.dec.conv1 = nn.Conv2d(64, 1, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+        self.sig = torch.nn.Sigmoid()
+
+
+    def encode(self, x):
+        x = self.enc(x)
+
+        x = F.relu(self.enc_fc(x))
+        mu = self.fc_mu(x)
+        log_var = self.fc_logvar(x)
+        return mu, log_var
+
+
+    def sampling(self, mu, log_var):
+        std = torch.exp(log_var * 0.5)
+        q = torch.distributions.Normal(mu, std)
+        z = q.rsample()
+        return z
+
+    def decode(self, z):
+
+        x = self.dec(z)
+        x = self.sig(x)
+        return x
+
+    def forward(self, x):
+        mu, _ = self.encode(x)
+        return mu
+
+    def log_density_gaussian(self, x: Tensor, mu: Tensor, logvar: Tensor):
+        norm = - 0.5 * (math.log(2 * math.pi) + logvar)
+        log_density = norm - 0.5 * ((x - mu) ** 2 * torch.exp(-logvar))
+        return log_density
+
+    def loss(self, recons, x, mu, log_var, z):
+        # Inspired by: https://github.com/YannDubs/disentangling-vae/blob/7b8285baa19d591cf34c652049884aca5d8acbca/disvae/models/losses.py#L316
+        recons = torch.where(torch.isnan(recons), torch.zeros_like(recons), recons)
+        recons = torch.where(torch.isinf(recons), torch.zeros_like(recons), recons)
+
+        recons_loss = F.binary_cross_entropy(
+            recons.view(-1, self.hparams.input_dim**2).clamp(0,1).type(torch.FloatTensor),
+            x.view(-1, self.hparams.input_dim**2).clamp(0,1).type(torch.FloatTensor),
+            reduction='sum')
+
+        log_q_zx = self.log_density_gaussian(z, mu, log_var).sum(dim=1)
+
+        zeros = torch.zeros_like(z)
+        log_p_z = self.log_density_gaussian(z, zeros, zeros).sum(dim=1)
+
+        batch_size, latent_dim = z.shape
+        mat_log_q_z = self.log_density_gaussian(z.view(batch_size, 1, latent_dim),
+                                                mu.view(1, batch_size,
+                                                        latent_dim),
+                                                log_var.view(1, batch_size, latent_dim))
+
+
+        # Estimate the three KL terms (log(q(z))) via importance sampling
+        strat_weight = (self.hparams.trainset_size - batch_size + 1) / \
+            (self.hparams.trainset_size * (batch_size - 1))
+
+        importance_weights = torch.Tensor(batch_size, batch_size).fill_(
+            1 / (batch_size - 1)).to(x.device)
+
+        importance_weights.view(-1)[::batch_size] = 1 / self.hparams.trainset_size
+        importance_weights.view(-1)[1::batch_size] = strat_weight
+        importance_weights[batch_size - 2, 0] = strat_weight
+        log_importance_weights = importance_weights.log()
+
+        mat_log_q_z += log_importance_weights.view(batch_size, batch_size, 1)
+
+        log_q_z = torch.logsumexp(mat_log_q_z.sum(2), dim=1, keepdim=False)
+        log_prod_q_z = torch.logsumexp(
+            mat_log_q_z, dim=1, keepdim=False).sum(1)
+
+        # Three KL Term components
+        mi_loss = (log_q_zx - log_q_z).mean()
+        tc_loss = (log_q_z - log_prod_q_z).mean()
+        kld_loss = (log_prod_q_z - log_p_z).mean()
+
+        if self.training:
+            self.num_iter += 1
+            anneal_rate = min(0 + 1 * self.num_iter /
+                              self.hparams.anneal_steps, 1)
+        else:
+            anneal_rate = 1
+
+        loss = recons_loss/batch_size + \
+            self.hparams.alpha * mi_loss + \
+            self.hparams.beta * tc_loss + \
+            self.hparams.gamma * anneal_rate * kld_loss
+
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        x, _ = batch
+     
+        mu, log_var = self.encode(x)
+        z = self.sampling(mu, log_var)
+     
+        recons = self.decode(z)
+   
+        loss = self.loss(recons, x, mu, log_var, z)
+
+        self.log('loss', loss, on_epoch=False, prog_bar=True, on_step=True, 
+            sync_dist=True if torch.cuda.device_count() > 1 else False)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, _ = batch
+
+        mu, log_var = self.encode(x)
+        z = self.sampling(mu, log_var)
+
+        recons = self.decode(z)
+
+
+        val_loss = self.loss(recons, x, mu, log_var, z)
+
+        self.log('val_loss', val_loss, on_epoch=True, prog_bar=True, 
+            sync_dist=True if torch.cuda.device_count() > 1 else False)
+
+        return val_loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr, weight_decay = self.hparams.weight_decay)
+
+        scheduler = {'scheduler': CosineAnnealingLR(optimizer,T_max=self.trainer.max_epochs, verbose=True), 
+                    'interval': 'epoch'}
+        return [optimizer], [scheduler] 
+
+    def get_progress_bar_dict(self):
+        tqdm_dict = super().get_progress_bar_dict()
+        if 'v_num' in tqdm_dict:
+            del tqdm_dict['v_num']
+        return tqdm_dict
