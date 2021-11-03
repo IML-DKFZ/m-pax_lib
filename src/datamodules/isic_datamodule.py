ebfd7cd52dd51ed4cd5c129244ce7938d3fb17e8
Lukas Klein <lukas.klein@dkfz-heidelberg.de>
2021-11-03 09:42:35 +0100

    Remove OOD Test, changes in ISIC

--- src/datamodules/isic_datamodule.py
+++ src/datamodules/isic_datamodule.py
@@ -52,7 +52,7 @@ class ISICDataset(Dataset):
 
         targets = self.targets.iloc[index, 1:]
         targets = np.array([targets])
-        targets = targets.reshape(-1, 8).argmax()
+        targets = targets.reshape(-1, 9).argmax()
         return img, targets
 
     def __len__(self):
@@ -121,14 +121,8 @@ class ISICDataModule(pl.LightningDataModule):
             self.test,
         ) = random_split(
             data,
-            [20000, 2918, 250, 81, 2000],
+            [20000, 2818, 432, 81, 2000],
             generator=torch.Generator().manual_seed(self.seed),
-        )
-
-        self.ood_test = ISICDataset(
-            img_path=self.data_dir + "/ISIC/ISIC_2019_Training_Input",
-            transform=transform_img,
-            csv_path=self.data_dir + "/ISIC/labels_ood.csv",
         )
 
         #### Computing and distributing weights for weighted sampling ####
@@ -183,14 +177,6 @@ class ISICDataModule(pl.LightningDataModule):
     def test_dataloader(self):
         return DataLoader(
             self.test,
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-            pin_memory=self.pin_memory,
-        )
-
-    def ood_test_dataloader(self):
-        return DataLoader(
-            self.ood_test,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             pin_memory=self.pin_memory,
